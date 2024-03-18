31e9ea98e1e17bef4457bb1560e6a37ffd0eba5e
rafaScalet <rafaelscalet2018@gmail.com>
2024-03-18 08:11:05 -0300

    'BootStrap', 'Icon BootStrap' and 'draw component' addition and configuration

---
+++ src/app/draw/draw.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-draw',
+  templateUrl: './draw.component.html',
+  styleUrl: './draw.component.css'
+})
+export class DrawComponent {
+
+}
