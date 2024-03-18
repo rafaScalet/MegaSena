bc13e827047e15f770de94d9f70bee86e6a84d16
rafaScalet <rafaelscalet2018@gmail.com>
2024-03-18 09:31:50 -0300

    the 'draw' function, now don't repeat any number

--- src/app/draw/draw.component.ts
+++ src/app/draw/draw.component.ts
@@ -1,3 +1,4 @@
+import { TestBed } from '@angular/core/testing';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,8 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './draw.component.css'
 })
 export class DrawComponent {
-  draw(num: number = 0){
-    num = Math.floor((Math.random() * 60) + 1);
-    alert(num);
+  winningNumbers: number[] = []
+
+  draw(){
+    //make a list with all the numbers for draw
+    const list: number[] = []
+    for(let i = 1; i <= 5; i++){
+      list.push(i)
+    }
+
+    //do the draw
+    const winningNumbers: number[] = []
+    for(let i = 0; i < 6; i++){
+      const index = Math.floor(Math.random() *list.length) //do the draw with the numbers on the list
+      const winningNumber = list.splice(index, 1)[0] //removes the drawn numbers to avoid repetion, and set on "winningNumber"
+      winningNumbers.push(winningNumber) //set on a arrray
+    }
+    this.winningNumbers = winningNumbers; //associate on a global variabel
   }
 }
